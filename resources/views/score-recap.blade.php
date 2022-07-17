0b35bed1e7cf56593158db7df3a954272970e603
ToastedBaguette <ronyhartonoirawan@gmail.com>
2022-07-17 10:01:30 +0700

    Update front end Rekap Penilaian

--- resources/views/score-recap.blade.php
+++ resources/views/score-recap.blade.php
@@ -6,37 +6,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('') }}assets/icons/logo.png">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('') }}assets/icons/logo.png">
-    <title>Upgrade Industrial Games 30</title>
+    <title>Rekap Penilaian Industrial Games 30</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
-<body>
-    <div class="row align-items-center">
+<body style="background-color: #FAF0DC;">
+    <div class="row align-items-center" style="background-color: #EA435E;">
         <div class="col-md-4 offset-md-4">
-            <h3 class="text-center fw-bolder m-2" style="color: #EA435E;">REKAP PENILAIAN</h3>
+            <h3 class="text-center text-white fw-bolder m-2">REKAP PENILAIAN</h3>
         </div>
         <div class="col-md-1 offset-md-3">
-            <button type="button" class=" btn btn-block btn-outline-danger m-2" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="text-danger">Logout</button>
+            <button type="button" class=" btn btn-block btn-danger shadow-sm m-2" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="text-danger">Logout</button>
             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
         </div>
     </div>
-    
-    <table class="table table-hover bg-white rounded my-1">
-        <thead>
-            <tr class="text-center align-middle">
-                <th scope="col">Nama Tim</th>
-                <th scope="col">Score</th>
-            </tr>
-        </thead>
-        <tbody>
-            @for ($i = 1; $i < 10; $i++)
-            <tr class="text-center align-middle">
-                <td class="w-25">Perusahaan {{$i}}</td>
-                <td class="w-25">0</td>
-            </tr>
-            @endfor
-        </tbody>
-    </table>
+    @for ($i = 1; $i <= 5; $i++)
+        <div class="row justify-content-center">
+            <h2 class="text-center"><span class="badge bg-dark rounded mt-3"> BATCH {{$i}} </span></h2>
+            <table class="table table-striped table-warning table-bordered mt-2 shadow-sm" style="width: 85%">
+                <thead>
+                    <tr class="text-center align-middle">
+                        <th scope="col">Nama Tim</th>
+                        <th scope="col">Six Sigma</th>
+                        <th scope="col">Profit</th>
+                        <th scope="col">Pangsa</th>
+                        <th scope="col">TOTAL</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @for ($j = 1; $j <= 10; $j++)
+                    <tr class="text-center align-middle">
+                        <td style="width: 40%;">Perusahaan {{$j}}</td>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>0</td>
+                    </tr>
+                    @endfor
+                </tbody>
+            </table>
+        </div>
+    @endfor
+
+   
 </body>
 </html>
