9d258802dfa7b9ebced87b2d0bdd75d19e7e4644
ToastedBaguette <ronyhartonoirawan@gmail.com>
2022-08-05 16:26:12 +0700

    Update Front End Demand Acara

--- resources/views/demand.blade.php
+++ resources/views/demand.blade.php
@@ -10,39 +10,88 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <style>
+        body {
+            background-image: url('{{ asset('')}}assets/img/background.jpg');
+            background-repeat: no-repeat;
+            background-attachment: fixed;  
+            background-size: cover;
+        }
+    </style>
+
 </head>
-<body style="background-color: #FAF0DC;">
-    <div class="row align-items-center bg-warning" style="box-shadow: 5px 2px 5px rgba(0, 0, 0, 0.5)">
-        <div class="col-md-1">
-            <button type="button" class=" btn btn-block btn-danger m-2" style="box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3)" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</button>
+<body>
+    {{-- Navbar --}}
+    <nav class="navbar justify-content-between px-3" style="background-color: #ffff; box-shadow: 5px 0px 5px rgba(0, 0, 0, 0.3);">
+            {{-- Logo IG --}}
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('assets') }}/icons/Logo_IG_Header.png" alt="Logo IGXXX" style="max-height: 40px">
+            </a>
+
+            <form class="form-inline">
+                {{-- Score Recap --}}
+                <a  href="{{ route('score-recap') }}"><button class="btn btn-outline-success my-sm-0" type="button">Score Recap</button></a>
+                {{-- Log out --}}
+                <button class="btn btn-outline-danger my-sm-0" type="button" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</button>
+            </form>
+            
             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
+    </nav>
+
+    <div class="row justify-content-center p-5">
+        <div class="col">
+            <div class="card bg-dark">
+                <div class="card-body">
+                    <h2 class="card-title text-center text-white">LEADERBOARD</h2>
+                    <h4 class="card-title text-center text-white">LEVEL SIGMA</h4>
+                    <ul class="list-group">
+                        @for ($i = 1; $i <= 10; $i++)
+                            <li class="list-group-item text-center fw-bold"><h4>Perusahaan {{$i}}</h4></li>
+                        @endfor
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div class="col-md-4 offset-md-3">
-            <h3 class="text-center fw-bolder m-2">DEMAND</h3>
-        </div>
-        <div class="col-md-1 offset-md-3">
-            <a href="{{ route('score-recap') }}"><button type="button" class=" btn btn-block btn-success m-2" style="box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3)" href="" onclick="">Recap</button></a>
-            <form id="demand-form" action="" method="POST" class="d-none">@csrf</form>
-        </div>
-    </div>
-    <div class="row justify-content-center">
-        <h2 class="text-center"><span class="badge bg-dark rounded mt-3" id="batch">BATCH {{ $batch }}</span></h2>
-        <table class="table table-warning table-bordered shadow-sm" style="width: 70%">
-            <thead>
-                <tr class="text-center align-middle">
-                    <th style="width : 50%;"><h1>Produk</h1></th>
-                    <th><h1>Demand Tersisa</h1></th>
-                </tr>
-            </thead>
-            <tbody id="tbody-demand">
-                @foreach ($demands as $demand)
+        <div class="col">
+            <div class="row mb-2">
+                <div class="col rounded-start bg-dark ms-2"><h2 class="text-center text-white py-2">BATCH  {{ $batch }}</h2></div>
+                <div class="col rounded-end bg-dark me-2"><h2 class="text-center text-white py-2">00:00</h2></div>
+            </div>
+         
+            <table class="table table-light table-bordered shadow-sm">
+                <thead>
                     <tr class="text-center align-middle">
-                        <td class="border-0 text-center align-middle"><h2>{{ $demand->name }}</h2></td>
-                        <td class="border-0 text-center align-middle"><h2>{{ $demand->pivot->amount }}</h2></td>
+                        <th style="width : 50%;"><h2 class="fw-bold" >Produk</h2></th>
+                        <th><h2 class="fw-bold">Sisa Demand</h2></th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody id="tbody-demand">
+                    @foreach ($demands as $demand)
+                        <tr class="text-center align-middle">
+                            <td class="border-0 text-center align-middle"><h3>{{ $demand->name }}</h3></td>
+                            <td class="border-0 text-center align-middle"><h3>{{ $demand->pivot->amount }}</h3></td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <table class="table table-warning table-bordered shadow-sm">
+                <thead>
+                    <tr class="text-center align-middle">
+                        <th style="width : 50%;"><h2 class="fw-bold">Produk</h2></th>
+                        <th><h2 class="fw-bold" >Harga</h2></th>
+                    </tr>
+                </thead>
+                <tbody id="tbody-demand">
+                    @foreach ($demands as $demand)
+                        <tr class="text-center align-middle">
+                            <td class="border-0 text-center align-middle"><h3>{{ $demand->name }}</h3></td>
+                            <td class="border-0 text-center align-middle"><h3>0 TC</h3></td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 
     <script src="../js/app.js"></script>
