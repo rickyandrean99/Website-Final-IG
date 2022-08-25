2f69812076a47ae4edcda156ab3da4204b1d5fbb
rickyandrean99 <84983109+rickyandrean99@users.noreply.github.com>
2022-08-25 21:57:09 +0700

    Update maintenance page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>No Access</title>
+</head>
+<body>
+    <div style="width: 100vw; height: 100vh; display: flex; justify-content: center; align-items: center">
+        <img src="{{ asset('assets/img/maintenance.jpg') }}" alt="maintenance" style="width: 50%; height: auto">
+    </div>
+</body>
+</html>
